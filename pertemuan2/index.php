e026c607f9b23fe1d64c19d673d9ee439888d415
Krisna-SP <krisnasuryap62@gmail.com>
2021-08-29 13:06:27 +0700

    Menambahkan folder pertemuan1 dan 2

---
+++ pertemuan2/index.php
@@ -0,0 +1,52 @@
+<?php
+// Pertemuan 2 - PHP Dasar
+// Sintaks PHP
+
+// Standar Output
+// echo, print
+// echo "Selamat Datang Krisna";
+// print_r
+// var_dump
+
+// Penulisan Sintaks PHP
+// 1. PHP di dalam HTML
+// 2. HTML di dalam PHP
+
+// Variabel dan Tipe Data
+// Variabel
+// tidak boleh di awali dengan angka, tapi boleh mangandung angaka.
+// $nama = "Krisna surya putra";
+// echo "Hello, nama saya $nama";
+
+// Operator
+// aritmatika
+// + - * / %
+// $x = 10;
+// $y = 20;
+// echo $x * $y;
+
+// penggabungan string / concat
+// .
+// $nama_depan = "Krisna";
+// $nama_belakang = "sp";
+// echo $nama_depan. " ". $nama_belakang;
+
+// Assignment
+// =, +=, -=, *=, %=, .=
+// $x = 10;
+// $y = 20;
+// echo $x * $y;
+// $nama_depan = "Krisna";
+// $nama_belakang = "sp";
+// echo $nama_depan. " ". $nama_belakang;
+
+// Perbandingan 
+// <, >, <=, >=, ==, !=
+// var_dump(1 === "1")
+
+// Logika
+// &&, ||, !
+$x = 30;
+var_dump($x < 20 || $x % 2 == 0);
+?>
+
