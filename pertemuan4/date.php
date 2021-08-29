38aa2dd4d9465ed0942fe60d6b3b9c11ab8ef72c
Krisna-SP <krisnasuryap62@gmail.com>
2021-08-29 22:55:32 +0700

    menambahkan folder pertemua4

---
+++ pertemuan4/date.php
@@ -0,0 +1,21 @@
+<?php
+// Date
+// Untuk menampilkan tanggal dengan format tertentu
+//echo date("l, d-M-Y");
+
+//Time
+// UNIX Timestamp / EPOCH time
+// detik yang sudah berlalu sejak 1 Januarai 1970
+// echo time();
+// echo date("d-M-Y", time()-60*60*24*100);
+
+// mktime
+// membuat sendiri detik
+// mktime(0,0,0,0,0,0,)
+// jam, menit, detik, bulan, tanggal, tahun
+// echo date("l", mktime(0,0,0, 3, 4, 2003));
+
+// strtotime
+echo date("l", strtotime("4 maret 2003"));
+ 
+?>
